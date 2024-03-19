d9abb9fa88defa47987b75ad90ab886b12f11d05
MihaiZegheru <mihai.z.2005@gmail.com>
2024-03-19 21:25:55 +0200

    Changed command_factory
    Signed-off-by: MihaiZegheru <mihai.z.2005@gmail.com>

--- include/command.h
+++ include/command.h
@@ -5,9 +5,10 @@
 #include <stdarg.h>
 
 #include <command_type.h>
+#include <command_data_object.h>
 #include <error_type.h>
 
-e_error_type_t command_factory(void *command_ptr, e_command_type_t command_type, ...);
+e_error_type_t command_factory(void *ptr, char *buffer);
 e_error_type_t command_destroy(void *command_ptr, e_command_type_t command_type);
 
 #endif // COMMAND_H__
--- src/command.c
+++ src/command.c
@@ -1,5 +1,13 @@
 #include <command.h>
 
-e_error_type_t command_factory(e_command_type_t command_type, ...) {
-	return ET_NONE;
+e_error_type_t command_factory(void *ptr, char *buffer) {
+	// size_t num_of_params = cdo_get_number_of_params(command_type);
+	// if (num_of_params != num) {
+	// 	// This is an error to be implemented
+	// 	return -1;
+	// }
+}
+
+e_error_type_t command_destroy(void *command_ptr, e_command_type_t command_type) {
+
 }
