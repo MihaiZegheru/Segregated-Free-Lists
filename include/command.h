bcbfcd3e8772f09f32b83aa214e287a24a65f614
MihaiZegheru <mihai.z.2005@gmail.com>
2024-03-19 20:41:49 +0200

    Added input_handler.h and input_handler.c
    Signed-off-by: MihaiZegheru <mihai.z.2005@gmail.com>

--- include/command.h
+++ include/command.h
@@ -7,6 +7,7 @@
 #include <command_type.h>
 #include <error_type.h>
 
-e_error_type_t command_factory(e_command_type_t command_type, ...);
+e_error_type_t command_factory(void *command_ptr, e_command_type_t command_type, ...);
+e_error_type_t command_destroy(void *command_ptr, e_command_type_t command_type);
 
 #endif // COMMAND_H__
---
+++ include/input_handler.h
@@ -0,0 +1,6 @@
+#ifndef INPUT_HANDLER_H__
+#define INPUT_HANDLER_H__
+
+#include <command.h>
+
+#endif // INPUT_HANDLER_H__
